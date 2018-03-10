7725bfc74ca85fe17ab58a2023b406df25f13ca2
Florian Duros <florian.duros@gmail.com>
2018-03-10 19:28:54 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","5d62c828aff833eb23fa038618f67efb"]];
+var precacheConfig = [["index.html","3e2efe1d8dd7bda4439b9b3e312e59e0"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
